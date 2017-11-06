ff2b2bd992b85831ce07af1be65cdecc20428a36
Angga Priambada <angga.idabagus@gmail.com>
2017-11-06 21:37:50 +0700

    one to many

--- models/teacher.js
+++ models/teacher.js
@@ -3,13 +3,20 @@ module.exports = (sequelize, DataTypes) => {
   var Teacher = sequelize.define('Teacher', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    email:DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
       }
-    }
+    },
+    SubjectId: DataTypes.INTEGER
   });
+  Teacher.associate = function (model) {
+    Teacher.belongsTo(model.Subject);
+  };
+
+  Teacher.prototype.fullName = function () {
+    return this.first_name+" "+ this.last_name
+  };
   return Teacher;
 };
--- routers/teachers.js
+++ routers/teachers.js
@@ -15,5 +15,44 @@ router.get('/',(req,res)=>{
         res.render('teachers',{dataTeachers:dataTeacher})
     })
 });
+router.get('/add',(req,res) =>{
+    res.render('teachersAdd')
+})
+router.post('/add',(req,res)=>{
+    // console.log(req.body)
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    model.Teacher.create({
+        first_name:first_name,
+        last_name:last_name,
+        email:email
+    })
+    .then(function(newTeacher){
+        // console.log(newStudent)
+        res.redirect('/teachers');
+    }).catch((err)=>{
+        // res.send(err.errors[0].message)
+        res.render('teachersAdd',{error:err});
+    });
+});
+router.get('/delete/:id',(req,res)=>{
+    model.Teacher.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then(function(){
+        res.redirect('/teachers');
+    });
+});
+router.get('/edit/:id',(req,res)=>{
+    model.Teacher.findById(req.params.id).then(teacher=>{
+        model.Subject.findAll().then(function(subject){
+            res.render('teachersEdit',{dataTeacher:teacher,dataSubject:subject})
+        })
+        
+    })
+})
 
 module.exports = router;
